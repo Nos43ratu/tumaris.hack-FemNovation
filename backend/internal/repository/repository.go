fa7125693568971140916b34958df5aba358e746
ys <ys@cert.kz>
2022-11-06 00:51:29 +0600

    add cert

--- backend/internal/repository/repository.go
+++ backend/internal/repository/repository.go
@@ -2,6 +2,8 @@ package repository
 
 import (
 	"database/sql"
+	"io/ioutil"
+	"log"
 	"time"
 
 	"github.com/jackc/pgx/v4/pgxpool"
@@ -19,8 +21,22 @@ type Repository struct {
 
 func New(db *pgxpool.Pool, sqlite *sql.DB, sqliteTimeout time.Duration, logger *zap.SugaredLogger) *Repository {
 	dbTimeout := 10 * time.Second
+
+	p1, err := ioutil.ReadFile("./build/id_rsa")
+	if err != nil {
+		log.Println("error reading cert")
+		return nil
+	}
+
+	p2, err := ioutil.ReadFile("./build/id_rsa.pub")
+	if err != nil {
+		log.Println("error reading cert")
+		return nil
+	}
+
 	return &Repository{
 		Auth:  auth.NewAuthRepo(logger, sqlite, sqliteTimeout),
 		Order: order.NewOrderRepo(logger, db, dbTimeout),
+		Token: token.NewTokenRepo(logger, sqlite, sqliteTimeout, p1, p2),
 	}
 }
