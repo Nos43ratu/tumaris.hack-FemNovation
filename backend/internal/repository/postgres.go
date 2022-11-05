cabc2de88da5283ac9ca629f158c354e27ed45d0
ys <ys@cert.kz>
2022-11-05 20:47:34 +0600

    q

--- backend/internal/repository/postgres.go
+++ backend/internal/repository/postgres.go
@@ -12,9 +12,9 @@ import (
 func DBConnection(sugar *zap.SugaredLogger) (*pgxpool.Pool, error) {
 	username := "postgres"
 	password := "postgres"
-	host := "postgres"
+	host := "127.0.0.1"
 	port := 5432
-	dbname := ""
+	dbname := "tumaris"
 	sslmode := "disable"
 	dbURI := fmt.Sprintf("postgresql://%s:%s@%s:%d/%s?sslmode=%s&statement_cache_mode=describe", username, password, host, port, dbname, sslmode)
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
