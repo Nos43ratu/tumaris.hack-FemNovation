835501775a8005fc1d8066a3ec77070b59020245
nnickie23 <tnazerke@wtotem.com>
2022-11-05 23:55:35 +0600

    delete

--- backend/internal/repository/products/sqlite.go
+++ backend/internal/repository/products/sqlite.go
@@ -43,3 +43,21 @@ func (r *ProductsRepo) CreateProduct(product *models.Product) (int, error) {
 
 	return id, nil
 }
+
+func (r *ProductsRepo) UpdateProduct(product *models.Product) (int, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), r.timeout)
+	var id int
+	defer cancel()
+
+	err := r.db.QueryRowContext(ctx, "INSERT INTO product (shop_id, name, description, sizes, colors, weight, price, category_id) VALUES ($1, $2, $3, $4, $5, $6, $7)",
+	product.ShopID, product.Name, product.Description, pq.Array(product.Sizes), pq.Array(product.Colors), product.Weight, product.Price, product.CategoryID).Scan(&id)
+	if err != nil {
+		r.logger.Errorf("db error: %s", err)
+		if errors.Is(err, pgx.ErrNoRows) {
+			return -1, models.ErrNoRows
+		}
+		return -1, err
+	}
+
+	return id, nil
+}
